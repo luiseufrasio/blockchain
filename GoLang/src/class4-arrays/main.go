03f5d92d2c6637a2ca06a0595e8eee92d8a58285
luiseufrasio <luis.eufrasio@gmail.com>
2022-11-14 07:53:30 -0300

    dynamic size

--- GoLang/src/class4-arrays/main.go
+++ GoLang/src/class4-arrays/main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	lista := [3]int{5, 25, 75}
+	lista := [...]int{5, 25, 75}
 
 	/*
+	lista := [3]int{5, 25, 75}
 	lista[0] = 5
 	lista[1] = 25
 	lista[2] = 75
