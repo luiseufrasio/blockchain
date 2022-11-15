6d9b17fd2f45f73d8c53d0c46fb708926e0f48e1
luiseufrasio <luis.eufrasio@gmail.com>
2022-11-15 07:53:00 -0300

    aritmetic ops

---
+++ GoLang/src/class6-op-arit/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	soma := 3 + 6
+
+	println(soma)
+
+	sub := 6 - 3
+
+	println(sub)
+
+	modulo := 5 % 2
+
+	println(modulo)
+
+	num := 1
+	num++
+	println(num)
+	num--
+	println(num)
+
+	num *= 9
+	println(num)
+}
