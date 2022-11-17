4c0ce674709da8e62aa258466ef71127cd54f05c
luiseufrasio <luis.eufrasio@gmail.com>
2022-11-17 09:04:30 -0300

    summing terms

--- GoLang/src/class10-functions/main.go
+++ GoLang/src/class10-functions/main.go
@@ -1,10 +1,35 @@
 package main
 
+// por referência
+func main() {
+	message := "Hey there!"
+	hello(&message)
+	println(message)
+
+	println(sum(1, 9, 10, 8))
+}
+
+func hello(message *string) {
+	println(*message)
+
+	*message = "Hey UPDATED!"
+}
+
+func sum(terms ...int) int {
+	result := 0
+	for _, term := range terms {
+		result += term
+	}
+	return result
+}
+
+/* por valor
 func main() {
 	message := "Hey there!"
 	hello(message)
 }
 
 func hello(message string) {
-	println("Hey!")
+	println(message)
 }
+*/
